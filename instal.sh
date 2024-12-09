d9f659906de4ed14bd97069cf6f03da025ae8ad0
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-12-09 21:40:41 +0700

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+apt-get update
+apt-get install curl
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
+./msfinstall
